a0849d5bd1d50757bdeb86a25b1679db4d1e6658
ilia <ilyakut@gmail.com>
2020-01-08 19:20:24 +0300

    added ActiveQuiz and AnswersList components

---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './AnswersList.module.css'
+
+const AnswersList = props => (
+  <ul className={classes.AnswersList}>
+    <li>1</li>
+  </ul>
+);
+
+export default AnswersList;
