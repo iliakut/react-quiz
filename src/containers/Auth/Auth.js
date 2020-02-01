234f1cdf18f2cc939e2dd630e81b8ae3e447b69c
ilia <ilyakut@gmail.com>
2020-02-01 14:58:03 +0300

    create input

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,47 @@
 import React, {Component} from 'react';
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button.js'
 
 class Auth extends Component {
+
+  loginHandler = () => {
+
+  };
+
+  signinHandler = () => {
+
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={classes.Auth}>
+        <div>
+          <h1>Auth</h1>
+          <form
+            onSubmit={this.submitHandler}
+            className={classes.AuthForm}
+          >
+            <input type="test"/>
+            <input type="test"/>
+
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+            >
+              Log in
+            </Button>
+            <Button
+              type="primary"
+              onClick={this.signinHandler}
+            >
+              Sign in
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
