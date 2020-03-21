725e4813c6d800efd2db9e011876920ebd0947d0
ilia <ilyakut@gmail.com>
2020-03-21 20:16:56 +0300

    added logout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import Layout from './hoc/Layout/Layout.js'
 import Quiz from './containers/Quiz/Quiz.js'
 import QuizList from "./containers/QuizList/QuizList";
 import Auth from "./containers/Auth/Auth";
 import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import {connect} from "react-redux";
+import Logout from "./components/Logout/Logout";
 
 function App() {
-  return (
-    <Layout>
+
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth}/>
+      <Route path="/quiz/:id" component={Quiz}/>
+      <Route path="/" component={QuizList}/>
+      <Redirect to="/"/>
+    </Switch>
+  );
+
+  if (this.props.isAuthentificated) {
+    routes = (
       <Switch>
-        <Route path="/auth" component={Auth}/>
         <Route path="/quiz-creator" component={QuizCreator}/>
         <Route path="/quiz/:id" component={Quiz}/>
         <Route path="/" component={QuizList}/>
+        <Route path="/logout" component={Logout}/>
+        <Redirect to="/"/>
       </Switch>
+    );
+  }
+
+  return (
+    <Layout>
+      {routes}
     </Layout>
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthentificated: !!state.audio.token
+  }
+}
+
+export default connect(mapStateToProps)(App);
